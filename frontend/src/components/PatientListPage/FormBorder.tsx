32327e6901b4d2d62cb35b57152d2eddcc36d96c
Atte <atte.lappalainen@hotmail.com>
2024-06-12 00:20:16 +0300

    add frontend run command, hide new entrt form

---
+++ frontend/src/components/PatientListPage/FormBorder.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@mui/material";
+import { useState } from "react"
+
+interface Props {
+    children: JSX.Element[]
+}
+
+export const FormBorder = ({children}: Props): JSX.Element => {
+    const [buttonstate, setButtonState] = useState<boolean>(false); //  is visible?
+
+    const showButtontext = () => {
+        if (buttonstate) {
+            return <div>
+                Hide
+            </div>  
+        }
+        return <div>
+                add new entry
+            </div>  
+    }
+
+
+    return (
+        <div>
+            <Button onClick={() => setButtonState(!(buttonstate))}>
+                {showButtontext()}
+            </Button>
+            <div style={{display: buttonstate? '' : 'none'}}>
+                {children}
+            </div>
+        </div>
+    )
+}
